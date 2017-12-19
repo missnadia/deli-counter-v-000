7284eacb837f352594a39ca859acf38f0d073e4f
Nadia Lee <miss.nadia.lee@gmail.com>
2017-12-19 21:16:06 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,12 @@
 def line(katz_deli)
-  if katz_deli.empty?
-    puts "The line is currently empty."
-  else
-    current_line = "The line is currently: "
-    katz_deli.each.with_index(1) { |name, number| current_line << "#{number}. #{name}"
+    if katz_deli.empty?
+      puts "The line is currently empty."
+    else
+      current_line = "The line is currently: "
+      katz_deli.each.with_index(1) { |name, number| current_line << "#{number}. #{name}"
+    end
+    puts current_line
   end
-  puts current_line
 end
 
 def take_a_number(katz_deli, name)
